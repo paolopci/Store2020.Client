37832ed7950175a96238ef5774735874a83e1d85
paolopci <paolopci@yahoo.it>
2020-05-18 17:46:33 +0200

    Complete Chapter 6: complete CRUD for product and supplier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Repository } from "./models/repository";
 import { Product } from "./models/product.model";
+import { Supplier } from "./models/supplier.model";
 
 
 @Component({
@@ -11,7 +12,7 @@ import { Product } from "./models/product.model";
 export class AppComponent {
   title = 'Store2020';
 
-  constructor(private repo: Repository) { }
+  constructor(private repo: Repository) {}
 
   get product(): Product {
     return this.repo.product;
@@ -19,5 +20,40 @@ export class AppComponent {
 
   get products(): Product[] {
     return this.repo.products;
+  }
+
+  createProduct() {
+    this.repo.createProduct(new Product(0,
+      "X-Ray Scube Mask",
+      "Watersports",
+      "See what the fish are hiding",
+      49.99,
+      this.repo.products[0].supplier));
+  }
+
+  createProductAndSupplier() {
+    let s = new Supplier(0, "Rocket Shoe Corp", "Boston", "MA");
+    let p = new Product(0, "Rocket-Powered Shoes", "Running", "Set a new record", 100, s);
+    this.repo.createProductAndSupplier(p, s);
+  }
+
+  replaceProduct() {
+    let p = this.repo.products[0];
+    p.name = "Modified Product";
+    p.category = "Modified Category";
+    this.repo.replaceProduct(p);
+  }
+
+  replaceSupplier() {
+    let s = new Supplier(3, "Modified Supplier", "New York", "NY");
+    this.repo.replaceSupplier(s);
+  }
+
+  deleteProduct() {
+    this.repo.deleteProduct(1);
+  }
+
+  deleteSupplier() {
+    this.repo.deleteSupplier(2);
   }
 }
--- src/app/models/repository.ts
+++ src/app/models/repository.ts
@@ -2,25 +2,26 @@ import { Product } from "./product.model";
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Filter } from "./configClasses.repository";
-
+import { Supplier } from "./supplier.model";
 
 const productUrl = "/api/products";
-
+const supplierUrl = "/api/suppliers";
 
 @Injectable()
 export class Repository {
-
   product: Product;
   // Chapter 5
   products: Product[];
-  filter: Filter=new Filter();
+  filter: Filter = new Filter();
+  // Chapter 6
+  suppliers: Supplier[]=[];
 
   constructor(private http: HttpClient) {
-    this.filter.category = 'soccer';
+    //this.filter.category = 'soccer';
     this.filter.related = true;
     this.getProducts();
   }
-
+  // Get - Get Product, Supplier and List Products,Suppliers
   getProduct(id: number) {
     this.http.get<Product>("/api/products/" + id).subscribe(p => {
       this.product = p;
@@ -29,7 +30,6 @@ export class Repository {
   }
 
   getProducts() {
-
     let url = `${productUrl}?related=${this.filter.related}`;
     if (this.filter.category) {
       url += `&category=${this.filter.category}`;
@@ -42,5 +42,74 @@ export class Repository {
     this.http.get<Product[]>(url).subscribe(prods => this.products = prods);
   }
 
+  getSuppliers() {
+    this.http.get<Supplier[]>(supplierUrl).subscribe(sups => this.suppliers = sups);
+  }
 
+  // Post - Create Product and Supplier
+  createProduct(prod: Product) {
+    let data = {
+      name: prod.name,
+      category: prod.category,
+      description: prod.description,
+      price: prod.price,
+      supplier: prod.supplier ? prod.supplier.supplierId : 0
+    };
+
+    this.http.post<number>(productUrl, data).subscribe(id => {
+      prod.productId = id;
+      this.products.push(prod);
+    });
+  }
+
+  createProductAndSupplier(prod: Product, supp: Supplier) {
+    let data = {
+      name: supp.name,
+      city: supp.city,
+      state: supp.state
+    };
+
+    this.http.post<number>(supplierUrl, data).subscribe(id => {
+      supp.supplierId = id;
+      prod.supplier = supp;
+      this.suppliers.push(supp);
+      if (prod != null) {
+        this.createProduct(prod);
+      }
+    });
+  }
+  // Update - Update Product and Supplier
+  replaceProduct(prod: Product) {
+    let data = {
+      name: prod.name,
+      category: prod.category,
+      description: prod.description,
+      price: prod.price,
+      supplier: prod.supplier ? prod.supplier.supplierId : 0
+    };
+    this.http.put(`${productUrl}/${prod.productId}`, data).subscribe(() => this.getProducts());
+  }
+
+  replaceSupplier(supp: Supplier) {
+    let data = {
+      name: supp.name,
+      city: supp.city,
+      state: supp.state
+    };
+
+    this.http.put(`${supplierUrl}/${supp.supplierId}`, data).subscribe(() => this.getProducts());
+
+  }
+
+  // Delete - Delete Product and Supplier
+  deleteProduct(id: number) {
+    this.http.delete(`${productUrl}/${id}`).subscribe(() => this.getProducts());
+  }
+
+  deleteSupplier(id: number) {
+    this.http.delete(`${supplierUrl}/${id}`).subscribe(() => {
+      this.getProducts();
+      this.getSuppliers();
+    });
+  }
 }
